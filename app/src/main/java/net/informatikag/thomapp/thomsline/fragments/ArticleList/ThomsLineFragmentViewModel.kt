cd243a1b8e2cc20a07950e0fc23ab8e39044dcde
isiko <koerner@informatik-ag.net>
2021-11-05 17:26:42 +0100

    Cleaned up ThomsLineFragmentViewModel.kt

--- app/src/main/java/net/informatikag/thomapp/thomsline/fragments/ArticleList/ThomsLineFragmentViewModel.kt
+++ app/src/main/java/net/informatikag/thomapp/thomsline/fragments/ArticleList/ThomsLineFragmentViewModel.kt
@@ -6,16 +6,12 @@ import net.informatikag.thomapp.thomsline.utils.WordpressArticle
 
 class ThomsLineFragmentViewModel(application: Application): AndroidViewModel(application) {
 
-    private val defaultImageURL = "https://thoms-line.thomaeum.de/wp-content/uploads/2021/01/Thom-01.jpg"
-
     private val _articles = MutableLiveData<ArrayList<ArrayList<WordpressArticle>>>()
     val articles: LiveData<ArrayList<ArrayList<WordpressArticle>>> = _articles
 
     var lastPage: Int = -1
 
     fun setArticles(pArticles: ArrayList<ArrayList<WordpressArticle>>){
-        _articles.apply {
-            value = pArticles
-        }
+        _articles.value = pArticles
     }
 }
