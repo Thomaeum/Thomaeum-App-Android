ef67d3690f3939e86e23ccd59b73a79d6f2bba25
isiko <koerner@informatik-ag.net>
2021-11-05 22:35:54 +0100

    Implemented Reloading via SwipeRefresh Layout
    Fixes #10
    Fixes #5

--- app/src/main/java/net/informatikag/thomapp/vertretungsplan/utils/VertretungsplanHandler.kt
+++ app/src/main/java/net/informatikag/thomapp/vertretungsplan/utils/VertretungsplanHandler.kt
@@ -1,16 +1,40 @@
 package net.informatikag.thomapp.vertretungsplan.utils
 
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import net.informatikag.thomapp.R
 import net.informatikag.thomapp.databinding.FragmentVertretungsplanTemplateBinding
 
 class VertretungsplanHandler(
     pdfURL: String,
-    layout: FragmentVertretungsplanTemplateBinding
-) {
+    val layout: FragmentVertretungsplanTemplateBinding
+): SwipeRefreshLayout.OnRefreshListener, WebViewClient() {
     init {
         layout.vertretungsplanOberstufePdfView.settings.loadWithOverviewMode = true
         layout.vertretungsplanOberstufePdfView.settings.javaScriptEnabled = true
         layout.vertretungsplanOberstufePdfView.loadUrl("https://docs.google.com/gview?embedded=true&url=$pdfURL")
+        layout.vertretungsplanOberstufePdfView.webViewClient = this
+
+
+        layout.root.setOnRefreshListener(this)
+        layout.root.setColorSchemeResources(
+            R.color.primaryColor,
+            R.color.secondaryColor
+        )
+        layout.root.post{
+            layout.root.isRefreshing = true
+        }
+    }
+
+    override fun onRefresh() {
+        layout.vertretungsplanOberstufePdfView.reload()
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        layout.root.post{
+            layout.root.isRefreshing = false
+        }
     }
 }
