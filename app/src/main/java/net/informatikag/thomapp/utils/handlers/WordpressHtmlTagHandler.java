5e1362a003a0d9fba2b76703749524bd4f3c296b
isiko <koerner@informatik-ag.net>
2021-11-08 21:29:35 +0100

    Compressed WordpressHtmlTagHandler

--- app/src/main/java/net/informatikag/thomapp/utils/handlers/WordpressHtmlTagHandler.java
+++ app/src/main/java/net/informatikag/thomapp/utils/handlers/WordpressHtmlTagHandler.java
@@ -14,33 +14,15 @@ public class WordpressHtmlTagHandler implements Html.TagHandler {
     public void handleTag(boolean opening, String tag, Editable output,
                           XMLReader xmlReader) {
         if(tag.equalsIgnoreCase("figure")) {
-            handleTag(opening, output);
-        }
-    }
+            if(!opening) {
+                String[] split = output.toString().split("\n");
+                int end = output.length();
+                int start = output.length() - 2*(split[split.length - 2].length()+1) - (split[split.length - 1].length()-1);
 
-    private void handleTag(boolean opening, Editable output) {
-        output.append("\n");
-        int end = output.length();
-        if(opening) {
-        } else {
-            String[] split = output.toString().split("\n");
-            int start = output.length() - 2*(split[split.length - 2].length()+1) - (split[split.length - 1].length()-1);
-
-            if (start < end) {
-                output.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), start, end, 0);
-            }
-        }
-    }
-
-    private Object getLast(Editable text, Class kind) {
-        Object[] objs = text.getSpans(0, text.length(), kind);
-        if (objs.length != 0) {
-            for(int i = objs.length;i>0;i--) {
-                if(text.getSpanFlags(objs[i-1]) == Spannable.SPAN_MARK_MARK) {
-                    return objs[i-1];
+                if (start < end) {
+                    output.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), start, end, 0);
                 }
             }
         }
-        return null;
     }
 }
