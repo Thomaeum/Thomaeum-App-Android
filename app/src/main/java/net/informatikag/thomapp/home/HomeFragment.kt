722773b9bdfa86374725c60ddc1884a978fe296c
isiko <koerner@informatik-ag.net>
2021-11-08 16:58:00 +0100

    Removed unused Elements generated by Android Studio

--- app/src/main/java/net/informatikag/thomapp/home/HomeFragment.kt
+++ app/src/main/java/net/informatikag/thomapp/home/HomeFragment.kt
@@ -12,7 +12,6 @@ import net.informatikag.thomapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,16 +23,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        binding.textHome.text = "This is home Fragment"
         return root
     }
 
