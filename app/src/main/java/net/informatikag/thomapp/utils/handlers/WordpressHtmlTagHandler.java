4b628b7e3fc515468d5bae221918a8ec78391a98
isiko <koerner@informatik-ag.net>
2021-11-08 22:21:59 +0100

    Migrated Tag Handler to Kotlin

--- app/src/main/java/net/informatikag/thomapp/utils/handlers/WordpressHtmlTagHandler.java
+++ app/src/main/java/net/informatikag/thomapp/utils/handlers/WordpressHtmlTagHandler.java
@@ -9,20 +9,18 @@ import android.text.style.StrikethroughSpan;
 
 import org.xml.sax.XMLReader;
 
-public class WordpressHtmlTagHandler implements Html.TagHandler {
-    @Override
-    public void handleTag(boolean opening, String tag, Editable output,
-                          XMLReader xmlReader) {
-        if(tag.equalsIgnoreCase("figure")) {
-            if(!opening) {
-                String[] split = output.toString().split("\n");
-                int end = output.length();
-                int start = output.length() - 2*(split[split.length - 2].length()+1) - (split[split.length - 1].length()-1);
-
-                if (start < end) {
-                    output.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), start, end, 0);
-                }
-            }
-        }
-    }
-}
+//public class WordpressHtmlTagHandler implements Html.TagHandler {
+//    @Override
+//    public void handleTag(boolean opening, String tag, Editable output,
+//                          XMLReader xmlReader) {
+//        if(tag.equalsIgnoreCase("figure") && !opening) {
+//            output.append("\n");
+//
+//            String[] split = output.toString().split("\n");
+//            int start = output.length() - 2*(split[split.length - 2].length()+1) - (split[split.length - 1].length()-1);
+//            int end = output.length();
+//
+//            if (start < end) { output.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), start, end, 0); }
+//        }
+//    }
+//}
