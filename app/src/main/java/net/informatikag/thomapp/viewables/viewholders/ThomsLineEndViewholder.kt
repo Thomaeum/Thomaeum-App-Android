68f3cf9830f0e7d23430366d10ccff150c0e1c97
isiko <koerner@informatik-ag.net>
2021-11-13 18:49:53 +0100

    Added Comments for Codeless Viewholders (ThomsLineEndViewholder.kt and ThomsLineLoadingViewholder.kt)

--- app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineEndViewholder.kt
+++ app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineEndViewholder.kt
@@ -3,4 +3,7 @@ package net.informatikag.thomapp.viewables.viewholders
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * Viewholder der agezeigt wird wenn das Ende des RecyclerViews erreicht ist
+ */
 class ThomsLineEndViewholder constructor(itemView: View): RecyclerView.ViewHolder(itemView)
--- app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineLoadingViewholder.kt
+++ app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineLoadingViewholder.kt
@@ -3,4 +3,7 @@ package net.informatikag.thomapp.viewables.viewholders
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * Viewholder der angezeigt wird wenn neue Artikel geladen werden
+ */
 class ThomsLineLoadingViewholder constructor(itemView: View): RecyclerView.ViewHolder(itemView)
