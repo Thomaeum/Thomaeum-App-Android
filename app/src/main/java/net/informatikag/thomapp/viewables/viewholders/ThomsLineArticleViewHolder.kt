7c916b7fa1bb22a160d45ee5c0ac13805e430d48
isiko <koerner@informatik-ag.net>
2021-11-13 18:56:20 +0100

    Added Comments for Article Viewholder

--- app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineArticleViewHolder.kt
+++ app/src/main/java/net/informatikag/thomapp/viewables/viewholders/ThomsLineArticleViewHolder.kt
@@ -11,14 +11,28 @@ import net.informatikag.thomapp.R
 import net.informatikag.thomapp.viewables.fragments.ThomsLine.main.ThomsLineFragment
 import net.informatikag.thomapp.utils.models.data.ThomsLineWordpressArticle
 
+/**
+ * Viewholder der einen Artikel anzeigt
+ * @param itemView Layout des Viewholders
+ * @param fragment Fragment in dem der Viewholer angezeigt wird. Das wird für den Context und das
+ *  Navigieren zur Detail Ansicht des Artikels benötigt
+ */
 class ThomsLineArticleViewHolder constructor(
     itemView: View,
     val fragment: ThomsLineFragment
 ): RecyclerView.ViewHolder(itemView){
+
+    /**
+     * Diese Methode wird aufgerufen wenn ein Artikel an den Viewholder gebunden wird, hier werden
+     * also die Inhalte in das Layout geladen
+     */
     fun bind(post: ThomsLineWordpressArticle){
+
+        // Artikel und Unterüberschrift setzen
         itemView.findViewById<TextView>(R.id.thomsline_post_title).setText(post.title)
         itemView.findViewById<TextView>(R.id.thomsline_post_excerpt).setText(post.excerpt)
 
+        // Wenn es eine ImageURL gibt wird diese geladen, ansonsten wird das standard Bild eingefügt
         if (post.imageURL != null)
         Glide.with(itemView.context)
             .applyDefaultRequestOptions(
@@ -34,6 +48,7 @@ class ThomsLineArticleViewHolder constructor(
                 R.drawable.img_thomsline_article_image_default
             ))
 
+        // Ein OnClickListener wird hinzugefügt, um bei einem Click in die Detailansicht wechseln zu können
         itemView.setOnClickListener(View.OnClickListener {
             fragment.onItemClick(post)
         })
