439093871672361220d8ba44623b920b4a919942
isiko <koerner@informatik-ag.net>
2021-11-13 13:54:43 +0100

    Added Comments for ThomsLineFragmentViewModel.kt

--- app/src/main/java/net/informatikag/thomapp/utils/models/view/ThomsLineFragmentViewModel.kt
+++ app/src/main/java/net/informatikag/thomapp/utils/models/view/ThomsLineFragmentViewModel.kt
@@ -4,13 +4,19 @@ import android.app.Application
 import androidx.lifecycle.*
 import net.informatikag.thomapp.utils.models.data.ThomsLineWordpressArticle
 
+/**
+ * Speichert die WordpressArtikel des ThomsLine Fragments
+ */
 class ThomsLineFragmentViewModel(application: Application): AndroidViewModel(application) {
 
+    //Die Artikel
     private val _articles = MutableLiveData<ArrayList<ArrayList<ThomsLineWordpressArticle>>>()
     val articles: LiveData<ArrayList<ArrayList<ThomsLineWordpressArticle>>> = _articles
 
+    //Die Letze Seite der Artikel,
     var lastPage: Int = -1
 
+    //Sets a Page
     fun setArticlePage(id: Int, content:ArrayList<ThomsLineWordpressArticle>){
         if (_articles.value == null) _articles.value = ArrayList(0)
 
@@ -20,6 +26,7 @@ class ThomsLineFragmentViewModel(application: Application): AndroidViewModel(app
         _articles.postValue(_articles.value)
     }
 
+    //Removes pages after the indexed Page
     fun removeArticlePagesFromIndex(index:Int){
         if (articles.value != null) while (_articles.value!!.size > index) _articles.value!!.removeLast()
     }
