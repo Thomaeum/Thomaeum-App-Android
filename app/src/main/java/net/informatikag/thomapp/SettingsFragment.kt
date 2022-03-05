83a093fdf7ffdd44199f06a811a19a143e4c9a10
isiko <koerner@informatik-ag.net>
2022-03-05 16:43:44 +0100

    Added Substitution preferences and Icons

--- app/src/main/java/net/informatikag/thomapp/SettingsFragment.kt
+++ app/src/main/java/net/informatikag/thomapp/SettingsFragment.kt
@@ -1,11 +1,19 @@
 package net.informatikag.thomapp
 
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        findPreference<Preference>("preferences_substitution_login")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                //TODO add Login process here
+                Log.d("login", "LOGING IN")
+                true
+            }
     }
 }
